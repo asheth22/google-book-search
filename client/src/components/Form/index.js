24f48f6f13777be8c711fd740f0201cbec97ea14
Anjali Sheth <anjali.sheth@otis.com>
2021-04-27 12:28:54 -0400

    added styling to main page

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -6,7 +6,10 @@ return (
   <div className="container">
   <form>
     <div className="form-group">
-      <label htmlFor="search"><h2>Search for Books of Interest and Save </h2></label>
+        {/* <label htmlFor="search"><h2>Search for Books of Interest and Save </h2></label> */}
+        <h1 style={{ fontSize: "1.5em", color: "green" }}>
+              Something Very <strong > Magical</strong> can happen when you read a <strong> Book!</strong>  <i> -- J.K. Rowlings</i>
+            </h1>
       <input
         onChange={props.handleInputChange}
         value={props.search}
